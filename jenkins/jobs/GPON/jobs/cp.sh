2716d56298219b96ca1f7655b354baaad26340c9
nick_huang <nick_huang@edge-core.com>
2020-05-27 13:18:13 +0800

    $1

---
+++ jenkins/jobs/GPON/jobs/cp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp -r voltha@172.17.10.60:~/jenkin-user-data/Jenkins_Jobs_bak/jobs-asgvolt64/* ./
--- pre_env.sh
+++ pre_env.sh
@@ -2,3 +2,5 @@
 docker build -t psme-jenkins . --no-cache
 PD=`pwd`
 docker run --user 1000:1000 -d  -p 8080:8080 -p 50000:50000 -v /$PD/jenkins:/var/jenkins_home psme-jenkins /sbin/tini -- /usr/local/bin/jenkins.sh
+#docker ps -a
+#docker exec -it d672f161ae08 /bin/bash
