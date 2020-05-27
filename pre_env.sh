10a4549ddee5808a403426b85287136343143bd9
nick_huang <nick_huang@edge-core.com>
2020-05-27 11:48:49 +0800

    1.Add docker env procedure

--- pre_env.sh
+++ pre_env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker build -t psme-jenkins . --no-cache
-#docker run --user 1000:1000 -d --restart always -p 8080:8080 -p 50000:50000 -v /jenkins:/var/jenkins_home psme-jenkins /sbin/tini -- /usr/local/bin/jenkins.sh 
-docker run --user 1000:1000 -d  -p 8080:8080 -p 50000:50000 -v /jenkins:/var/jenkins_home psme-jenkins /sbin/tini -- /usr/local/bin/jenkins.sh 
-
+PD=`pwd`
+docker run --user 1000:1000 -d  -p 8080:8080 -p 50000:50000 -v /$PD/jenkins:/var/jenkins_home psme-jenkins /sbin/tini -- /usr/local/bin/jenkins.sh
